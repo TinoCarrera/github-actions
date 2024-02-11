df722c6d1b7ef8a9a07a55c21cdf175d850042d9
Florentino Carrera <florentinocarrera44@gmail.com>
2024-02-11 22:34:05 +0100

    Creando función de División y su Test.

---
+++ division.py
@@ -0,0 +1,7 @@
+def dividir(a, b):
+    if b == 0:
+        return "No se puede dividir entre cero"
+    return a / b
+
+if __name__ == "__main__":
+    print(dividir(16, 2))
---
+++ test_division.py
@@ -0,0 +1,11 @@
+import unittest
+from division import dividir
+
+class TestSumar(unittest.TestCase):
+    def test_sumar(self):
+        self.assertEqual(dividir(3, 2), 1.5)
+        self.assertEqual(dividir(10, 5), 2.0)
+        self.assertEqual(dividir(5, 0), "No se puede dividir entre cero")
+
+if __name__ == '__main__':
+    unittest.main()
